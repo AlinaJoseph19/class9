f4c8756d9c719eeb23b118237d162eff0ee29cee
AlinaJoseph19 <125725233+AlinaJoseph19@users.noreply.github.com>
2023-02-23 16:07:40 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,43 @@
-var ball 
+
 function setup() {
   createCanvas(400,400);
-  ball = createSprite(200,200,20,20)
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
 function draw() 
 {
-  background(30);
-  if(keyDown("right")){
-    ball.x = ball.x + 2
-    ball.y = ball.y + 0
+
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
   }
-  if(keyDown("left")){
-    ball.x = ball.x - 2
-    ball.y = ball.y + 0
-  }
-  if(keyDown("up")){
-    ball.x = ball.x + 0
-    ball.y = ball.y + 2
-  }
-  if(keyDown("down")){
-    ball.x = ball.x + 0
-    ball.y = ball.y - 2
-  }
- drawSprites()
  
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+
+  }
+  
+    if (keyIsDown(RIGHT_ARROW))
+  {
+     background("red")
+
+  }
+  
+  drawSprites();
 }
-
-
-
 
